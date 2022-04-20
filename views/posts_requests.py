fd53717bb390b911f8455e5b8c3f7696df96f7a6
Noah Taggart <noahtag@gmail.com>
2022-04-20 15:59:17 -0500

    get_all_posts now has joined user and category data. ordered by publication_date

--- views/posts_requests.py
+++ views/posts_requests.py
@@ -1,6 +1,6 @@
 import sqlite3
 import json
-from models import Post
+from models import Post, Category, User
 
 
 def get_all_posts():
@@ -10,15 +10,14 @@ def get_all_posts():
 
         db_cursor.execute("""
         SELECT
-            p.id,
-            p.user_id,
-            p.category_id,
-            p.title,
-            p.publication_date,
-            p.image_url,
-            p.content,
-            p.approved
+            *
+            
         from Posts p
+        Join Categories c
+            ON c.id = p.category_id
+        Join Users u
+            on u.id = p.user_id
+        ORDER BY p.publication_date DESC
         """)
 
         posts = []
@@ -28,7 +27,17 @@ def get_all_posts():
         for row in dataset:
             post = Post(row['id'], row['user_id'], row['category_id'], row['title'],
                         row['publication_date'], row['image_url'], row['content'], row['approved'])
-
+            
+            category = Category(
+                row['id'], row['label']
+            )
+            
+            post.category = category.__dict__
+            
+            user = User(row['id'], row['first_name'], row['last_name'], row['email'], row['bio'], row['username'], row['password'], row['profile_image_url'], row['created_on'], row['active'] )
+            
+            post.user = user.__dict__
+            
             posts.append(post.__dict__)
 
     return json.dumps(posts)
