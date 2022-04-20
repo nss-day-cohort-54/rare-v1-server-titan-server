4c817d0247d4193e87df175a0b70ec818bc5d8c2
Noah Taggart <noahtag@gmail.com>
2022-04-20 14:43:56 -0500

    fixed server to output in a style javascript would like

--- models/post.py
+++ models/post.py
@@ -1,10 +1,10 @@
 class Post():
     def __init__(self, id, user_id, category_id, title, publication_date, image_url, content, approved ):
         self.id = id
-        self.user_id = user_id
-        self.category_id = category_id
+        self.userId = user_id
+        self.categoryId = category_id
         self.title = title
-        self.publication_date = publication_date
-        self.image_url = image_url
+        self.publicationDate = publication_date
+        self.imageURL = image_url
         self.content = content
         self.approved = approved
